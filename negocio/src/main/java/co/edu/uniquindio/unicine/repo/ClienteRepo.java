28640fa783091d6a1798999f1d15f0a586cb2ef5
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-09-24 20:58:33 -0500

    Se han creado algunos repositorios para el modelo

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/ClienteRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/ClienteRepo.java
@@ -1,2 +1,10 @@
-package co.edu.uniquindio.unicine.repo;public interface ClienteRepo {
+package co.edu.uniquindio.unicine.repo;
+
+import co.edu.uniquindio.unicine.entidades.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepo extends JpaRepository<Cliente, Integer> {
+
 }
