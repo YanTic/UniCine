7df53dd6ac9481536b521a6b4f0bae43a5e464b2
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-09-27 12:51:15 -0500

    Se han creado los repositorios para la mayoria de entidades

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/BoletaRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/BoletaRepo.java
@@ -1,9 +1,9 @@
 package co.edu.uniquindio.unicine.repo;
 
-import co.edu.uniquindio.unicine.entidades.Pelicula;
+import co.edu.uniquindio.unicine.entidades.Boleta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PeliculaRepo extends JpaRepository<Pelicula, Integer> {
+public interface BoletaRepo {
 }
