e924bd666f720d1bf7265503fb64a555305f9a9f
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-10-02 20:23:35 -0500

    Ahora los repositorios tienen consultas 'Query' al dataset

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/ClienteRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/ClienteRepo.java
@@ -1,10 +1,26 @@
 package co.edu.uniquindio.unicine.repo;
 
 import co.edu.uniquindio.unicine.entidades.Cliente;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClienteRepo extends JpaRepository<Cliente, Integer> {
 
+    @Query("select c from Cliente c where c.email = ?1")
+    Cliente obtener(String email);
+
+    Cliente findByEmail(String email); //Spring boot crea la consulta por inferencia
+
+    @Query("select c from Cliente c where c.email = :email and c.contrasenia = :contrasenia")
+    Cliente comprobarAutenticacion(String email, String contrasenia);
+
+    Cliente findByEmailAndContrasenia(String email, String contrasenia); // Metodo por inferencia
+
+    @Query("select c from Cliente c where c.estado = :estado")
+    List<Cliente> obtenerPorEstado(boolean estado, Pageable paginador);
 }
--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/TeatroRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/TeatroRepo.java
@@ -2,8 +2,16 @@ package co.edu.uniquindio.unicine.repo;
 
 import co.edu.uniquindio.unicine.entidades.Teatro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeatroRepo extends JpaRepository<Teatro, Integer> {
+
+    // Esto -> t.ciudad.nombre . Es imposible hacerlo en SQL, tendría que usar un JOIN
+    // Por lo que Hibernate realiza la traduccion por nosotros
+    @Query("select t from Teatro t where t.ciudad.nombre = :nombreCiudad")
+    List<Teatro> listar(String nombreCiudad);
 }
