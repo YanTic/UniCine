0d77a550f5116689ce662ceb3291a6b6638d4a55
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-11-01 21:05:12 -0500

    Precio de boletas y compras actualizado

--- negocio/src/test/resources/dataset.sql
+++ negocio/src/test/resources/dataset.sql
@@ -131,15 +131,15 @@ insert into funcion values (8, 330, 5, 4, 8);
 insert into funcion values (9, 240, 4, 1, 9);
 
 /*insert into compra values (id,fecha,metodopago,valortotal,clientecedula,cuponClienteid,funcionid );*/
-insert into compra values (1, '2022-05-03', 'EFECTY',    55200,  1, 1, 2);
-insert into compra values (2, '2022-05-03', 'EFECTY',    40200,  1, 2, 2);
-insert into compra values (3, '2022-05-03', 'VISA',      17000,  3, null, 1);
-insert into compra values (4, '2022-05-03', 'DAVIPLATA', 15500,  4, 5, 4);
-insert into compra values (5, '2022-06-10', 'EFECTY',    25000,  2, 3, 5);
-insert into compra values (6, '2022-06-12', 'EFECTY',    50300,  5, null, 9);
-insert into compra values (7, '2022-06-24', 'VISA',      35080,  3, null, 1);
-insert into compra values (8, '2022-07-13', 'EFECTY',    130170, 2, null, 3);
-insert into compra values (9, '2022-07-17', 'NEQUI',     35012,  4, 6, 8);
+insert into compra values (1, '2022-05-03', 'EFECTY',    42200,  1, 1, 2);
+insert into compra values (2, '2022-05-03', 'EFECTY',    30120,  1, 2, 2);
+insert into compra values (3, '2022-05-03', 'VISA',      40200,  3, null, 1);
+insert into compra values (4, '2022-05-03', 'DAVIPLATA', 52462,  4, 5, 4);
+insert into compra values (5, '2022-06-10', 'EFECTY',    6240,  2, 3, 5);
+insert into compra values (6, '2022-06-12', 'EFECTY',    51240,  5, null, 9);
+insert into compra values (7, '2022-06-24', 'VISA',      45400,  3, null, 1);
+insert into compra values (8, '2022-07-13', 'EFECTY',    120240, 2, null, 3);
+insert into compra values (9, '2022-07-17', 'NEQUI',     10264,  4, 6, 8);
 
 /*insert into confiteria_compra values (id,precio,unidades,compraid,confiteria_id);*/
 insert into confiteria_compra values (1, 30000, 2, 1, 1);
@@ -156,16 +156,16 @@ insert into confiteria_compra values (11, 40000, 4, 8, 3);
 insert into confiteria_compra values (12, 10000, 1, 9, 3);
 
 /*insert into boleta values (id, col, fila, precio, tipo, compraid);*/
-insert into boleta values (1, "2", "A", 200, 'ESTANDAR', 1);
-insert into boleta values (2, "2", "B", 200, 'ESTANDAR', 1);
-insert into boleta values (3, "3", "A", 2200, 'VIP', 2);
-insert into boleta values (4, "4", "A", 4150, 'ESTANDAR', 3);
-insert into boleta values (5, "2", "F", 3050, 'ESTANDAR', 4);
-insert into boleta values (6, "6", "A", 1000, 'VIP', 4);
-insert into boleta values (7, "8", "E", 1000, 'ESTANDAR', 5);
-insert into boleta values (8, "1", "F", 2200, 'ESTANDAR', 6);
-insert into boleta values (9, "1", "G", 5200, 'DISCAPACITADO', 7);
-insert into boleta values (10, "7", "A", 3200, 'VIP', 7);
-insert into boleta values (11, "4", "B", 5600, 'ESTANDAR', 8);
-insert into boleta values (12, "4", "C", 1100, 'DISCAPACITADO', 9);
+insert into boleta values (1, "2", "A", 100, 'ESTANDAR', 1);
+insert into boleta values (2, "2", "B", 100, 'ESTANDAR', 1);
+insert into boleta values (3, "3", "A", 120, 'VIP', 2);
+insert into boleta values (4, "4", "A", 200, 'ESTANDAR', 3);
+insert into boleta values (5, "2", "F", 210, 'ESTANDAR', 4);
+insert into boleta values (6, "6", "A", 252, 'VIP', 4);
+insert into boleta values (7, "8", "E", 150, 'ESTANDAR', 5);
+insert into boleta values (8, "1", "F", 240, 'ESTANDAR', 6);
+insert into boleta values (9, "1", "G", 160, 'DISCAPACITADO', 7);
+insert into boleta values (10, "7", "A", 240, 'VIP', 7);
+insert into boleta values (11, "4", "B", 240, 'ESTANDAR', 8);
+insert into boleta values (12, "4", "C", 264, 'DISCAPACITADO', 9);
 
