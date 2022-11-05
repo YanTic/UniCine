55de75d2a1b055f158e8ee4a1814425f9d459c75
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-11-05 13:58:01 -0500

    Pruebas con xhtml y PrimeFaces

--- web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/InicioBean.java
@@ -13,9 +13,13 @@ public class InicioBean implements Serializable {
 
     @Getter @Setter
     private String msg = "Mi primera pagina en JSF";
-    // A diferencia de los htmls, que son archivos que si se cambian cuando el servidor está en
-    // ejecucion, al refrescar la pagina, esos cambios se ven; los mensajes que vienen el controlador
-    // no se actualizarán automaticamente porque vienen del lado del servidor (no de la vista), por
-    // lo que tienen ser compilados y subidos al servidor (tomcat)
 
+    @Getter @Setter
+    private String dato1, dato2;
+
+    public void cambiarValores(){
+        String aux = dato1;
+        dato1 = dato2;
+        dato2 = aux;
+    }
 }
