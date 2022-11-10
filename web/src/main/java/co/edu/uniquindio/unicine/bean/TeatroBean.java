f4c74cf51c9f1875b2b4484b19d53fea57556691
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-11-10 11:20:49 -0500

    Nuevos nombre para los archivos y funcionalidades ajax para los botones de la toolbar de crudTeatro

--- web/src/main/java/co/edu/uniquindio/unicine/bean/TeatroBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/TeatroBean.java
@@ -13,6 +13,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -39,6 +40,7 @@ public class TeatroBean implements Serializable {
         teatro = new Teatro();
         ciudades = adminGeneralServicio.listarCiudades();
         teatros = adminGeneralServicio.listarTeatros();
+        teatrosSeleccionados = new ArrayList<>();
     }
 
     public void crearTeatro() {
@@ -46,11 +48,35 @@ public class TeatroBean implements Serializable {
             Teatro registro = adminGeneralServicio.crearTeatro(teatro);
             teatros.add(registro); // Actualizamos los datos de la tabla que está en la web
 
+            teatro = new Teatro(); // Cuando se crea el objeto, se tiene que resetear (porque en la vista sigue siendo el mismo)
+
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Informacion", "Teatro creado con exito!");
             FacesContext.getCurrentInstance().addMessage("mensaje_bean", fm);
         } catch (Exception e) {
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", e.getMessage());
             FacesContext.getCurrentInstance().addMessage("mensaje_bean", fm);
+        }
+    }
+
+    public void eliminarTeatros() {
+        teatrosSeleccionados.forEach(t -> {
+            try {
+                adminGeneralServicio.eliminarTeatro(t.getId());
+                teatros.remove(t);
+                teatrosSeleccionados.clear();
+            } catch (Exception e) {
+                FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", e.getMessage());
+                FacesContext.getCurrentInstance().addMessage("mensaje_bean", fm);
+            }
+        });
+    }
+
+    public String getMsgBtnEliminar() {
+        if(teatrosSeleccionados.isEmpty()) {
+            return "Eliminar";
+        }
+        else {
+            return teatrosSeleccionados.size() == 1 ? "Eliminar: 1 elemento" : "Eliminar: "+teatrosSeleccionados.size()+" elementos";
         }
     }
 }
