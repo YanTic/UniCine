c6f9e9ce2cfd12dc11ae856d01b03ad82d9e3757
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-11-17 01:32:58 -0500

    detalle_pelicula.xhtml ahora muestra la desc de pelicula, img y trailer

--- web/src/main/java/co/edu/uniquindio/unicine/bean/DetallePeliculaBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/DetallePeliculaBean.java
@@ -1,8 +1,10 @@
 package co.edu.uniquindio.unicine.bean;
 
+import co.edu.uniquindio.unicine.entidades.Ciudad;
 import co.edu.uniquindio.unicine.entidades.Funcion;
 import co.edu.uniquindio.unicine.entidades.Pelicula;
 import co.edu.uniquindio.unicine.servicios.AdminGeneralServicio;
+import co.edu.uniquindio.unicine.servicios.AdminTeatroServicio;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +12,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -28,19 +33,43 @@ public class DetallePeliculaBean implements Serializable {
     private List<Funcion> funciones; // Traer las funciones dada la pelicula y la ciudad
                                      // Y dada la pelicula y ciudad, traer los teatros
 
+    @Getter @Setter
+    private List<Ciudad> ciudades;
+
+    @Getter @Setter
+    private Ciudad ciudad;
+
     @Autowired
     private AdminGeneralServicio adminGeneralServicio;
+
+    @Autowired
+    private AdminTeatroServicio adminTeatroServicio;
 
     @PostConstruct
     public void init() {
         try {
             if (!peliculaId.isEmpty() && peliculaId != null) {
                 pelicula = adminGeneralServicio.obtenerPelicula(Integer.parseInt(peliculaId));
+                ciudades = adminGeneralServicio.listarCiudades();
+                funciones = new ArrayList<>();
+                ciudad = new Ciudad();
             }
 
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }
 
+        }
     }
+
+
+    public void actualizarFunciones() {
+        try {
+            funciones = adminTeatroServicio.listarFuncionesPorPeliculaCiudad(Integer.parseInt(peliculaId), ciudad.getId());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", e.getMessage());
+            FacesContext.getCurrentInstance().addMessage("mensajes", fm);
+        }
+    }
+
 }
--- web/src/main/webapp/resources/js/index.js
+++ web/src/main/webapp/resources/js/index.js
@@ -1,4 +1,9 @@
 function pageScroll() {
     // window.scrollBy(0,50);
-    scrollTo(0, 40)
+    // scrollTo(0, 40)
+
+    // El scrollTO hace que la pagina inicie siempre en la parte superior, mas abajo del primer div ('barra_principal') -> (x:0, y:div.altura)
+    scrollTo(0, document.getElementById('barra_principal').clientHeight);
+
+    // Esto deberia ser implementado o eliminado, porque ya <body> llama está funcion sin necesidad del javascript
 }
