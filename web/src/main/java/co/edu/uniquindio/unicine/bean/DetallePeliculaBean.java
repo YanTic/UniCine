71d2d3ea7ba3be9ea0b0a93a69fe8772fbc3b0d4
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-11-17 12:10:06 -0500

    detalle pelicula ahora muestras las funciones-salas-peliculas asociadas

--- web/src/main/java/co/edu/uniquindio/unicine/bean/DetallePeliculaBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/DetallePeliculaBean.java
@@ -1,8 +1,6 @@
 package co.edu.uniquindio.unicine.bean;
 
-import co.edu.uniquindio.unicine.entidades.Ciudad;
-import co.edu.uniquindio.unicine.entidades.Funcion;
-import co.edu.uniquindio.unicine.entidades.Pelicula;
+import co.edu.uniquindio.unicine.entidades.*;
 import co.edu.uniquindio.unicine.servicios.AdminGeneralServicio;
 import co.edu.uniquindio.unicine.servicios.AdminTeatroServicio;
 import lombok.Getter;
@@ -16,6 +14,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,10 +33,19 @@ public class DetallePeliculaBean implements Serializable {
                                      // Y dada la pelicula y ciudad, traer los teatros
 
     @Getter @Setter
+    private List<Teatro> teatros;
+
+    @Getter @Setter
+    private List<Sala> salas;
+
+    @Getter @Setter
     private List<Ciudad> ciudades;
 
     @Getter @Setter
     private Ciudad ciudad;
+
+    @Getter @Setter
+    private List<LocalDate> fechas;
 
     @Autowired
     private AdminGeneralServicio adminGeneralServicio;
@@ -52,7 +60,11 @@ public class DetallePeliculaBean implements Serializable {
                 pelicula = adminGeneralServicio.obtenerPelicula(Integer.parseInt(peliculaId));
                 ciudades = adminGeneralServicio.listarCiudades();
                 funciones = new ArrayList<>();
+                teatros = new ArrayList<>();
+                salas = new ArrayList<>();
                 ciudad = new Ciudad();
+                fechas = new ArrayList<>();
+                llenarFechas();
             }
 
         } catch (Exception e) {
@@ -65,11 +77,55 @@ public class DetallePeliculaBean implements Serializable {
     public void actualizarFunciones() {
         try {
             funciones = adminTeatroServicio.listarFuncionesPorPeliculaCiudad(Integer.parseInt(peliculaId), ciudad.getId());
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", e.getMessage());
             FacesContext.getCurrentInstance().addMessage("mensajes", fm);
         }
+    }
+
+    public void actualizarTeatroSalas(LocalDate fecha) {
+        try {
+            teatros = adminGeneralServicio.listarTeatrosPorCiudadPeliculaFecha(ciudad.getId(), Integer.parseInt(peliculaId),fecha);
+            salas = adminTeatroServicio.listarSalasPorCiudadPeliculaFecha(ciudad.getId(), Integer.parseInt(peliculaId),fecha); // Es la misma consulta solo para obtener las salas
+
+            teatros.forEach(System.out::println);
+            salas.forEach(System.out::println);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", e.getMessage());
+            FacesContext.getCurrentInstance().addMessage("mensajes", fm);
+        }
+    }
+
+
+    public void llenarFechas() {
+        fechas.add(LocalDate.now());
+
+        for(int i = 1 ; i<7; i++) {
+            fechas.add(LocalDate.now().plusDays(i));
+        }
+
+        fechas.forEach(System.out::println);
+    }
+
+    public String getMsgBtnDiaFunciones(LocalDate fecha) {
+        String dia = fecha.getDayOfWeek().toString();
+        String fechaCompleta = ""+ fecha.getDayOfMonth()+" "+ fecha.getMonth().name()+" "+ fecha.getYear();
+
+        System.out.println(dia + "\n"+ fechaCompleta);
+
+        return dia + "\n"+ fechaCompleta;
+    }
+
+    public String getMsgTeatro(Teatro t) {
+        return t.getNombre() + " - "+ t.getDireccion();
+    }
+
+    public void realizarCompra() {
+        System.out.println("compre pues mor");
     }
 
 }
